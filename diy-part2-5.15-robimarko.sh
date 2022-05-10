4edac31b6f650c81f3450853df64a5878e6ecbf7
hnyyghk <i@5icodes.com>
2022-05-10 19:16:24 +0800

    smartdns添加block ad domain list

--- diy-part2-5.15-robimarko.sh
+++ diy-part2-5.15-robimarko.sh
@@ -57,86 +57,93 @@ touch package/base-files/files/etc/custom.tag
 sed -i '/exit 0/d' package/base-files/files/etc/rc.local
 cat >> package/base-files/files/etc/rc.local << EOFEOF
 fun() {
-    echo "1.sleep begin" >> /etc/custom.tag
+    PPPOE_USERNAME=""
+    PPPOE_PASSWORD=""
+    DDNS_USERNAME=""
+    DDNS_PASSWORD=""
+    SSR_SUBSCRIBE_URL=""
+    SSR_SAVE_WORDS=""
+
     sleep 30
-    echo "2.subscribe begin" >> /etc/custom.tag
-    /usr/bin/lua /usr/share/shadowsocksr/subscribe.lua >> /etc/custom.tag
-    echo "3.config begin" >> /etc/custom.tag
-    uci set shadowsocksr.cfg013fd6.global_server='cfg104a8f'
-    uci set shadowsocksr.cfg013fd6.pdnsd_enable='0'
-    uci del shadowsocksr.cfg013fd6.tunnel_forward
-    uci commit shadowsocksr
-    /etc/init.d/shadowsocksr restart >> /etc/custom.tag
-    echo "shadowsocksr finish" >> /etc/custom.tag
-}
 
-if [ -f "/etc/custom.tag" ];then
-    exit 0
-fi
-touch /etc/custom.tag
+    uci set network.wan.proto='pppoe'
+    uci set network.wan.username="\${PPPOE_USERNAME}"
+    uci set network.wan.password="\${PPPOE_PASSWORD}"
+    uci set network.wan.ipv6='auto'
+    uci set network.modem=interface
+    uci set network.modem.proto='dhcp'
+    uci set network.modem.device='eth0'
+    uci set network.modem.defaultroute='0'
+    uci set network.modem.peerdns='0'
+    uci set network.modem.delegate='0'
+    uci commit network
+    /etc/init.d/network restart >> /etc/custom.tag
+    echo "network finish" >> /etc/custom.tag
 
-PPPOE_USERNAME=""
-PPPOE_PASSWORD=""
-DDNS_USERNAME=""
-DDNS_PASSWORD=""
-SSR_SUBSCRIBE_URL=""
-SSR_SAVE_WORDS=""
+    sleep 30
 
-uci set network.wan.proto='pppoe'
-uci set network.wan.username="\${PPPOE_USERNAME}"
-uci set network.wan.password="\${PPPOE_PASSWORD}"
-uci set network.wan.ipv6='auto'
-uci set network.modem=interface
-uci set network.modem.proto='dhcp'
-uci set network.modem.device='eth0'
-uci set network.modem.defaultroute='0'
-uci set network.modem.peerdns='0'
-uci set network.modem.delegate='0'
-uci commit network
-/etc/init.d/network restart >> /etc/custom.tag
-echo "network finish" >> /etc/custom.tag
+    uci del firewall.cfg03dc81.network
+    uci add_list firewall.cfg03dc81.network='wan'
+    uci add_list firewall.cfg03dc81.network='wan6'
+    uci add_list firewall.cfg03dc81.network='modem'
+    uci commit firewall
+    /etc/init.d/firewall restart >> /etc/custom.tag
+    echo "firewall finish" >> /etc/custom.tag
 
-uci del firewall.cfg03dc81.network
-uci add_list firewall.cfg03dc81.network='wan'
-uci add_list firewall.cfg03dc81.network='wan6'
-uci add_list firewall.cfg03dc81.network='modem'
-uci commit firewall
-/etc/init.d/firewall restart >> /etc/custom.tag
-echo "firewall finish" >> /etc/custom.tag
+    uci set ttyd.cfg01a8ea.ssl='1'
+    uci set ttyd.cfg01a8ea.ssl_cert='/etc/nginx/conf.d/_lan.crt'
+    uci set ttyd.cfg01a8ea.ssl_key='/etc/nginx/conf.d/_lan.key'
+    uci commit ttyd
+    /etc/init.d/ttyd restart >> /etc/custom.tag
+    echo "ttyd finish" >> /etc/custom.tag
 
-uci set smartdns.cfg016bb1.enabled='1'
-uci set smartdns.cfg016bb1.server_name='smartdns'
-uci set smartdns.cfg016bb1.port='5335'
-uci set smartdns.cfg016bb1.tcp_server='0'
-uci set smartdns.cfg016bb1.ipv6_server='0'
-uci set smartdns.cfg016bb1.dualstack_ip_selection='1'
-uci set smartdns.cfg016bb1.prefetch_domain='1'
-uci set smartdns.cfg016bb1.serve_expired='1'
-uci set smartdns.cfg016bb1.redirect='redirect'
-uci set smartdns.cfg016bb1.cache_size='8192'
-uci set smartdns.cfg016bb1.rr_ttl='30'
-uci set smartdns.cfg016bb1.rr_ttl_min='30'
-uci set smartdns.cfg016bb1.rr_ttl_max='300'
-uci set smartdns.cfg016bb1.seconddns_port='6553'
-uci set smartdns.cfg016bb1.seconddns_tcp_server='0'
-uci set smartdns.cfg016bb1.seconddns_no_speed_check='0'
-uci set smartdns.cfg016bb1.seconddns_no_rule_addr='0'
-uci set smartdns.cfg016bb1.seconddns_no_rule_nameserver='0'
-uci set smartdns.cfg016bb1.seconddns_no_rule_ipset='0'
-uci set smartdns.cfg016bb1.seconddns_no_rule_soa='0'
-uci set smartdns.cfg016bb1.seconddns_no_dualstack_selection='0'
-uci set smartdns.cfg016bb1.seconddns_no_cache='0'
-uci set smartdns.cfg016bb1.force_aaaa_soa='0'
-uci set smartdns.cfg016bb1.coredump='0'
-uci del smartdns.cfg016bb1.old_redirect
-uci add_list smartdns.cfg016bb1.old_redirect='redirect'
-uci del smartdns.cfg016bb1.old_port
-uci add_list smartdns.cfg016bb1.old_port='5335'
-uci del smartdns.cfg016bb1.old_enabled
-uci add_list smartdns.cfg016bb1.old_enabled='1'
-uci commit smartdns
-cat >> /etc/smartdns/custom.conf << EOF
+    uci set autoreboot.cfg01f8be.enable='1'
+    uci set autoreboot.cfg01f8be.week='7'
+    uci set autoreboot.cfg01f8be.hour='3'
+    uci set autoreboot.cfg01f8be.minute='30'
+    uci commit autoreboot
+    /etc/init.d/autoreboot restart >> /etc/custom.tag
+    echo "autoreboot finish" >> /etc/custom.tag
 
+    uci set smartdns.cfg016bb1.enabled='1'
+    uci set smartdns.cfg016bb1.server_name='smartdns'
+    uci set smartdns.cfg016bb1.port='5335'
+    uci set smartdns.cfg016bb1.tcp_server='0'
+    uci set smartdns.cfg016bb1.ipv6_server='0'
+    uci set smartdns.cfg016bb1.dualstack_ip_selection='1'
+    uci set smartdns.cfg016bb1.prefetch_domain='1'
+    uci set smartdns.cfg016bb1.serve_expired='1'
+    uci set smartdns.cfg016bb1.redirect='dnsmasq-upstream'
+    uci set smartdns.cfg016bb1.cache_size='8192'
+    uci set smartdns.cfg016bb1.rr_ttl='30'
+    uci set smartdns.cfg016bb1.rr_ttl_min='30'
+    uci set smartdns.cfg016bb1.rr_ttl_max='300'
+    uci set smartdns.cfg016bb1.seconddns_port='6553'
+    uci set smartdns.cfg016bb1.seconddns_tcp_server='0'
+    uci set smartdns.cfg016bb1.seconddns_no_speed_check='0'
+    uci set smartdns.cfg016bb1.seconddns_no_rule_addr='0'
+    uci set smartdns.cfg016bb1.seconddns_no_rule_nameserver='0'
+    uci set smartdns.cfg016bb1.seconddns_no_rule_ipset='0'
+    uci set smartdns.cfg016bb1.seconddns_no_rule_soa='0'
+    uci set smartdns.cfg016bb1.seconddns_no_dualstack_selection='0'
+    uci set smartdns.cfg016bb1.seconddns_no_cache='0'
+    uci set smartdns.cfg016bb1.force_aaaa_soa='0'
+    uci set smartdns.cfg016bb1.coredump='0'
+    uci del smartdns.cfg016bb1.old_redirect
+    uci add_list smartdns.cfg016bb1.old_redirect='redirect'
+    uci del smartdns.cfg016bb1.old_port
+    uci add_list smartdns.cfg016bb1.old_port='5335'
+    uci del smartdns.cfg016bb1.old_enabled
+    uci add_list smartdns.cfg016bb1.old_enabled='1'
+    uci commit smartdns
+    touch /etc/smartdns/ad.conf
+    cat >> /etc/smartdns/custom.conf << EOF
+
+
+# Include another configuration options
+conf-file /etc/smartdns/ad.conf
+
+# remote dns server list
 server 114.114.114.114 #114DNS
 server 114.114.115.115 #114DNS
 server-tls 1.1.1.1 #CloudflareDNS
@@ -157,48 +164,156 @@ server-https https://doh.opendns.com/dns-query #OpenDNS
 server 180.76.76.76 #BaiduDNS
 server 2400:da00::6666 #BaiduDNS
 EOF
-/etc/init.d/smartdns restart >> /etc/custom.tag
-echo "smartdns finish" >> /etc/custom.tag
+    /etc/init.d/smartdns restart >> /etc/custom.tag
+    echo "smartdns remote dns server list finish" >> /etc/custom.tag
 
-uci set ttyd.cfg01a8ea.ssl='1'
-uci set ttyd.cfg01a8ea.ssl_cert='/etc/nginx/conf.d/_lan.crt'
-uci set ttyd.cfg01a8ea.ssl_key='/etc/nginx/conf.d/_lan.key'
-uci commit ttyd
-/etc/init.d/ttyd restart >> /etc/custom.tag
-echo "ttyd finish" >> /etc/custom.tag
+    sleep 30
 
-uci set autoreboot.cfg01f8be.enable='1'
-uci set autoreboot.cfg01f8be.week='7'
-uci set autoreboot.cfg01f8be.hour='3'
-uci set autoreboot.cfg01f8be.minute='30'
-uci commit autoreboot
-/etc/init.d/autoreboot restart >> /etc/custom.tag
-echo "autoreboot finish" >> /etc/custom.tag
+    uci set ddns.test=service
+    uci set ddns.test.service_name='cloudflare.com-v4'
+    uci set ddns.test.use_ipv6='1'
+    uci set ddns.test.enabled='1'
+    uci set ddns.test.lookup_host='test.5icodes.com'
+    uci set ddns.test.domain='test@5icodes.com'
+    uci set ddns.test.username="\${DDNS_USERNAME}"
+    uci set ddns.test.password="\${DDNS_PASSWORD}"
+    uci set ddns.test.ip_source='network'
+    uci set ddns.test.ip_network='wan_6'
+    uci set ddns.test.interface='wan_6'
+    uci set ddns.test.use_syslog='2'
+    uci set ddns.test.check_unit='minutes'
+    uci set ddns.test.force_unit='minutes'
+    uci set ddns.test.retry_unit='seconds'
+    uci commit ddns
+    /etc/init.d/ddns restart >> /etc/custom.tag
+    echo "ddns finish" >> /etc/custom.tag
 
-uci set ddns.test=service
-uci set ddns.test.service_name='cloudflare.com-v4'
-uci set ddns.test.use_ipv6='1'
-uci set ddns.test.enabled='1'
-uci set ddns.test.lookup_host='test.5icodes.com'
-uci set ddns.test.domain='test@5icodes.com'
-uci set ddns.test.username="\${DDNS_USERNAME}"
-uci set ddns.test.password="\${DDNS_PASSWORD}"
-uci set ddns.test.ip_source='network'
-uci set ddns.test.ip_network='wan_6'
-uci set ddns.test.interface='wan_6'
-uci set ddns.test.use_syslog='2'
-uci set ddns.test.check_unit='minutes'
-uci set ddns.test.force_unit='minutes'
-uci set ddns.test.retry_unit='seconds'
-uci commit ddns
-/etc/init.d/ddns restart >> /etc/custom.tag
-echo "ddns finish" >> /etc/custom.tag
+    uci add_list shadowsocksr.cfg029e1d.subscribe_url="\${SSR_SUBSCRIBE_URL}"
+    uci set shadowsocksr.cfg029e1d.save_words="\${SSR_SAVE_WORDS}"
+    uci set shadowsocksr.cfg029e1d.switch='1'
+    uci set shadowsocksr.cfg029e1d.auto_update_time='4'
+    uci commit shadowsocksr
+    /usr/bin/lua /usr/share/shadowsocksr/subscribe.lua >> /etc/custom.tag
+    uci set shadowsocksr.cfg013fd6.global_server='cfg104a8f'
+    uci set shadowsocksr.cfg013fd6.pdnsd_enable='0'
+    uci del shadowsocksr.cfg013fd6.tunnel_forward
+    uci commit shadowsocksr
+    /etc/init.d/shadowsocksr restart >> /etc/custom.tag
+    echo "shadowsocksr finish" >> /etc/custom.tag
 
-uci add_list shadowsocksr.cfg029e1d.subscribe_url="\${SSR_SUBSCRIBE_URL}"
-uci set shadowsocksr.cfg029e1d.save_words="\${SSR_SAVE_WORDS}"
-uci set shadowsocksr.cfg029e1d.switch='1'
-uci set shadowsocksr.cfg029e1d.auto_update_time='4'
-uci commit shadowsocksr
+    sleep 30
+
+    wget -c -P /etc/smartdns https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-smartdns.conf 2>> /etc/custom.tag
+    if [ -f "/etc/smartdns/anti-ad-smartdns.conf" ];then
+        grep "^address" /etc/smartdns/anti-ad-smartdns.conf >> /etc/smartdns/aaa.conf
+        rm -f /etc/smartdns/anti-ad-smartdns.conf
+    fi
+    wget -c -P /etc/smartdns https://raw.githubusercontent.com/neodevpro/neodevhost/master/smartdns.conf 2>> /etc/custom.tag
+    if [ -f "/etc/smartdns/smartdns.conf" ];then
+        grep "^address" /etc/smartdns/smartdns.conf >> /etc/smartdns/aaa.conf
+        rm -f /etc/smartdns/smartdns.conf
+    fi
+    wget -c -P /etc/smartdns https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts 2>> /etc/custom.tag
+    if [ -f "/etc/smartdns/hosts" ];then
+        grep "^127" /etc/smartdns/hosts > /etc/smartdns/host
+        sed -i '1d' /etc/smartdns/host
+        sed -i 's/ #-> 腾讯视频//g' /etc/smartdns/host
+        sed -i 's/ #-> 爱奇艺//g' /etc/smartdns/host
+        sed -i 's/ #-> 微信//g' /etc/smartdns/host
+        sed -i 's/127.0.0.1 /address \//g;s/$/&\/#/g' /etc/smartdns/host
+        cat /etc/smartdns/host >> /etc/smartdns/aaa.conf
+        rm -f /etc/smartdns/hosts
+        rm -f /etc/smartdns/host
+    fi
+    wget -c -P /etc/smartdns https://raw.githubusercontent.com/FuckNoMotherCompanyAlliance/Fuck_CJMarketing_hosts/master/hosts 2>> /etc/custom.tag
+    if [ -f "/etc/smartdns/hosts" ];then
+        grep "^0" /etc/smartdns/hosts > /etc/smartdns/host
+        sed -i 's/0.0.0.0 /address \//g;s/$/&\/#/g' /etc/smartdns/host
+        cat /etc/smartdns/host | tr -d "\r" >> /etc/smartdns/aaa.conf
+        rm -f /etc/smartdns/hosts
+        rm -f /etc/smartdns/host
+    fi
+    wget -c -P /etc/smartdns https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts 2>> /etc/custom.tag
+    if [ -f "/etc/smartdns/hosts" ];then
+        grep "^127" /etc/smartdns/hosts > /etc/smartdns/host
+        sed -i 's/127.0.0.1 /address \//g;s/$/&\/#/g' /etc/smartdns/host
+        cat /etc/smartdns/host | tr -d "\r" >> /etc/smartdns/aaa.conf
+        rm -f /etc/smartdns/hosts
+        rm -f /etc/smartdns/host
+    fi
+    wget -c -P /etc/smartdns https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts.txt 2>> /etc/custom.tag
+    if [ -f "/etc/smartdns/hosts.txt" ];then
+        grep "^0" /etc/smartdns/hosts.txt > /etc/smartdns/host.txt
+        sed -i 's/0.0.0.0 /address \//g;s/$/&\/#/g' /etc/smartdns/host.txt
+        cat /etc/smartdns/host.txt >> /etc/smartdns/aaa.conf
+        rm -f /etc/smartdns/hosts.txt
+        rm -f /etc/smartdns/host.txt
+    fi
+    cat >> /etc/smartdns/aaa.conf << EOF
+address /ad.xiaomi.com/#
+address /ad1.xiaomi.com/#
+address /ad.mi.com/#
+address /tat.pandora.xiaomi.com/#
+address /api.ad.xiaomi.com/#
+address /t7z.cupid.ptqy.gitv.tv/#
+address /fix.hpplay.cn/#
+address /rps.hpplay.cn/#
+address /imdns.hpplay.cn/#
+address /f5.market.mi-img.com/#
+address /devicemgr.hpplay.cn/#
+address /rp.hpplay.cn/#
+address /tvapp.hpplay.cn/#
+address /pin.hpplay.cn/#
+address /adcdn.hpplay.cn/#
+address /sl.hpplay.cn/#
+address /vipauth.hpplay.cn/#
+address /vipsdkauth.hpplay.cn/#
+address /sdkauth.hpplay.cn/#
+address /adeng.hpplay.cn/#
+address /conf.hpplay.cn/#
+address /image.hpplay.cn/#
+address /hotupgrade.hpplay.cn/#
+address /t7z.cupid.ptqy.gitv.tv/#
+address /cloud.hpplay.cn/#
+address /ad.hpplay.cn/#
+address /adc.hpplay.cn/#
+address /gslb.hpplay.cn/#
+address /cdn1.hpplay.cn/#
+address /ftp.hpplay.com.cn/#
+address /rp.hpplay.com.cn/#
+address /cdn.hpplay.com.cn/#
+address /userapi.hpplay.com.cn/#
+address /leboapi.hpplay.com.cn/#
+address /api.hpplay.com.cn/#
+address /h5.hpplay.com.cn/#
+address /hpplay.cdn.cibn.cc/#
+address /logonext.tv.kuyun.com/#
+address /config.kuyun.com/#
+address /f5.market.xiaomi.com/#
+address /f4.market.xiaomi.com/#
+address /f3.market.xiaomi.com/#
+address /f2.market.xiaomi.com/#
+address /f1.market.xiaomi.com/#
+address /video.market.xiaomi.com/#
+address /f5.market.mi-img.com/#
+address /f4.market.mi-img.com/#
+address /f3.market.mi-img.com/#
+address /f2.market.mi-img.com/#
+address /f1.market.mi-img.com/#
+EOF
+    sort -u /etc/smartdns/aaa.conf > /etc/smartdns/bbb.conf
+    echo "# block ad domain list" >> /etc/smartdns/ad.conf
+    cat /etc/smartdns/bbb.conf >> /etc/smartdns/ad.conf
+    rm -f /etc/smartdns/aaa.conf
+    rm -f /etc/smartdns/bbb.conf
+    /etc/init.d/smartdns restart >> /etc/custom.tag
+    echo "smartdns block ad domain list finish" >> /etc/custom.tag
+}
+
+if [ -f "/etc/custom.tag" ];then
+    exit 0
+fi
+touch /etc/custom.tag
 
 fun &
 echo "rc.local finish" >> /etc/custom.tag
