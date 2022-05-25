b1fe73b375a06f8acd59c6f551a2c9475f4651fc
hnyyghk <i@5icodes.com>
2022-05-25 13:02:04 +0800

    修复ccache不生效问题，显示隐藏文件

--- diy-part1-5.15-openwrt.sh
+++ diy-part1-5.15-openwrt.sh
@@ -17,23 +17,20 @@ git config --global user.email "i@5icodes.com"
 git config --global user.name "hnyyghk"
 git reset --hard 0d2d52df69e91fe2f6e3e65750c0728fc45f5ab2
 echo "openwrt: before rm -rf *"
-ls
-rm -rf *
+ls -a
+# Retain .git
+rm -rf .gitattributes .github .gitignore BSDmakefile COPYING Config.in LICENSES Makefile README.md config feeds.conf.default include package rules.mk scripts target toolchain tools
 echo "openwrt: after rm -rf *"
-ls
-# Retain .git and .ccache
-rm -rf .gitattributes .github .gitignore
-echo "openwrt: after rm -rf .gitattributes .github .gitignore"
-ls
+ls -a
 git clone https://github.com/robimarko/openwrt -b ipq807x-5.15-pr ../openwrt-temp
 echo "openwrt-temp: before rm -rf ../openwrt-temp/.git"
-ls ../openwrt-temp
+ls -a ../openwrt-temp
 rm -rf ../openwrt-temp/.git
 echo "openwrt-temp: after rm -rf ../openwrt-temp/.git"
-ls ../openwrt-temp
+ls -a ../openwrt-temp
 mv ../openwrt-temp/* ../openwrt-temp/.[^.]* ./
 echo "openwrt: after mv ../openwrt-temp/* ../openwrt-temp/.[^.]* ./"
-ls
+ls -a
 git add -A
 git commit -m "temp"
 git pull --rebase
