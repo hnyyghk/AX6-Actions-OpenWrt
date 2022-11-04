0ee0d059f79578ad5a67383b59cc9ea18d084f71
hnyyghk <i@5icodes.com>
2022-11-04 23:54:43 +0800

    delete Boos4721 source

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,15 +6,12 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1-5.15-imoutowrt.sh
+# File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-git config --global user.email "i@5icodes.com"
-git config --global user.name "hnyyghk"
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
