dd4a21398c3ad11fd6aba46690eb4ace90f16acc
hnyyghk <i@5icodes.com>
2024-10-22 20:11:05 +0800

    use custom nginx

--- diy-part2-ax6-openwrt.sh
+++ diy-part2-ax6-openwrt.sh
@@ -45,7 +45,6 @@ sed -i "/^PKG_SOURCE:=/cPKG_SOURCE_PROTO:=git\nPKG_SOURCE_URL:=https://github.co
 # nginx quic
 rm -rf feeds/packages/net/nginx
 rm -rf feeds/packages/net/nginx-util
-git clone -b upstream https://github.com/hnyyghk/OpenWrt_Nginx-QUIC feeds/packages/tmp
 
 # add alter inbound api
 #sed -i "/^PKG_SOURCE:=/s/.*//" feeds/packages/net/v2ray-core/Makefile
